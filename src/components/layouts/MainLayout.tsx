f80bbc384d5c0281ca505c0be5c4397a6e8d6a1a
hoon_ground <break_the_limit@naver.com>
2026-04-28 02:04:42 +0900

    [#5]Design: MainLayout에 Header, Footer 적용

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
+import { Header } from './Header';
 
 export default function MainLayout() {
   return (
     <div className="page-frame min-h-dvh">
       <div className="mx-auto flex min-h-dvh w-full max-w-[375px] flex-col">
+        <Header />
         <main className="flex-1">
           <Outlet />
         </main>
+        <Footer />
       </div>
     </div>
   );
