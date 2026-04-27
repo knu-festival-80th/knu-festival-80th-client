516cdd38d94af203e3b05b90b3e024d51ab4ab1b
hoon_ground <break_the_limit@naver.com>
2026-04-28 02:00:54 +0900

    [#5]Design: logo 소스 수정, Header 컴포넌트에 sticky적용

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,9 +2,9 @@ import { LuMenu } from 'react-icons/lu';
 
 export const Header = () => {
   return (
-    <header className="flex h-16 w-full items-center justify-between px-5">
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between bg-background px-5 shadow-sm">
       <img
-        src="/figma-assets/knu80th_logo.png"
+        src="/figma-assets/knu80th_logo_dark.png"
         alt="KNU 80주년 대동제"
         className="h-4.5 w-47.5 object-contain"
       />
