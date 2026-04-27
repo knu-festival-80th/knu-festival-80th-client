ac547c657cd9468b7122262cbf3890ad3074c810
hoon_ground <break_the_limit@naver.com>
2026-04-28 03:36:20 +0900

    [#5]Refactor: Footer 관심사분리

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,20 +1,5 @@
 import { Link } from 'react-router-dom';
-
-// TODO: 페이지 확정 후 각 경로 연결
-const NAV_LINKS = [
-  { label: '지도 정보', to: '/' },
-  { label: '롤링페이퍼', to: '/' },
-  { label: '인스타팅', to: '/' },
-  { label: '포토부스', to: '/' },
-  { label: '공지사항', to: '/' },
-] as const;
-
-// TODO: 페이지 확정 후 각 경로 연결
-const FOOTER_LINKS = [
-  { label: '개인정보 보호', to: '/' },
-  { label: '서비스 이용 약관', to: '/' },
-  { label: '쿠키 설정', to: '/' },
-] as const;
+import { FOOTER_LINKS, NAV_LINKS } from '../../constants/footer';
 
 export const Footer = () => {
   return (
