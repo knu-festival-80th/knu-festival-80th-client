a710d0451580f098bf3bab250c9792be77b03f8d
seongmin <sm4046@naver.com>
2026-04-29 10:39:59 +0900

    [#7]Feat: 페이지 이동 시 스크롤 위치를 상단으로 초기화

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,6 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export default function MainLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="page-frame min-h-dvh">
       <div className="mx-auto flex min-h-dvh w-full max-w-[375px] flex-col">
