b10e9fd37a0cfb47d1cda7e9937594a7ad04245b
seongmin <sm4046@naver.com>
2026-04-29 09:24:30 +0900

    [#7]Feat: TimeTablePage 뼈대 생성 및 /timetable 라우트 연결

---
+++ src/pages/TimeTablePage.tsx
@@ -0,0 +1,7 @@
+export default function TimeTablePage() {
+  return (
+    <div className="bg-surface flex flex-col min-h-dvh px-5 pt-16 pb-16">
+      <h1 className="text-heading1">Time Table</h1>
+    </div>
+  );
+}
