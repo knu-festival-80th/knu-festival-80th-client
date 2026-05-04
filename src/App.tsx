ea23fd5934a0f46ce488fbda97a570547256d927
seongmin <sm4046@naver.com>
2026-05-04 15:50:06 +0900

    [#25]Feat: 축전 영상 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import MainLayout from '@/components/layouts/MainLayout';
+import CongratVideoPage from '@/pages/CongratVideoPage';
 import HomePage from '@/pages/HomePage';
 import NotFoundPage from '@/pages/NotFoundPage';
 import TimeTablePage from '@/pages/TimeTablePage';
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="timetable" element={<TimeTablePage />} />
+          <Route path="congrat-video" element={<CongratVideoPage />} />
         </Route>
         <Route path="/404" element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
